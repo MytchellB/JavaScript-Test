d7b07a203d19c307f411bebba02bac828527de1f
MytchellB <mbeaton@csumb.edu>
2018-07-06 19:54:58 -0700

    Fixed text color for different BGs
    Fixed text color for different BGs

--- Page4/page4JS.js
+++ Page4/page4JS.js
@@ -4,12 +4,15 @@ function changeBG(newBG) {
 	switch(newBG) {
 		case "blackBG":
 			document.body.style.backgroundColor="black";
+			document.body.style.color="white";
 			break;
 		case "whiteBG":
 			document.body.style.backgroundColor="white";
+			document.body.style.color="black";
 			break;
 		case "blueBG":
 			document.body.style.backgroundColor="blue";
+			document.body.style.color="white";
 			break;
 	}
 }
