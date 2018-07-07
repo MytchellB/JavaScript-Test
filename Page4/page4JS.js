ed8b0b2864d3617529dc8f3550dbd4cfa33ebb72
MytchellB <mbeaton@csumb.edu>
2018-07-06 19:53:29 -0700

    Choose BG with drop down list on page 4
    Choose BG with drop down list on page 4

--- Page4/page4JS.js
+++ Page4/page4JS.js
@@ -1,2 +1,27 @@
 var i;
 
+function changeBG(newBG) {
+	switch(newBG) {
+		case "blackBG":
+			document.body.style.backgroundColor="black";
+			break;
+		case "whiteBG":
+			document.body.style.backgroundColor="white";
+			break;
+		case "blueBG":
+			document.body.style.backgroundColor="blue";
+			break;
+	}
+}
+
+function blackBG(){
+	document.body.style.backgroundColor="black";
+}
+
+function whiteBG(){
+	document.body.style.backgroundColor="white";
+}
+
+function blueBG(){
+	document.body.style.backgroundColor="blue";
+}
